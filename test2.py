18967c483bb306ec7c53b5d1909c482556f3949e
Tein van der Lugt <teinvdlugt@gmail.com>
2015-12-05 11:37:22 +0100

    PEP8 and stuff

--- test2.py
+++ test2.py
@@ -1,31 +1,33 @@
 from graphics import *
 import math
 
-class CarWindow(object):
 
+class CarWindow(object):
     def __init__(self):
         self.win = GraphWin(title="Self-driving car", width=600, height=400)
         self.rw = 20
+        self.road = []
+        self.lines = []
 
-    def setRoad(self, pts):
-        self.road = pts
-        self.makeLines()
+    def set_road(self, points):
+        self.road = points
+        self.make_lines()
 
-    def makeLines(self):
+    def make_lines(self):
         self.lines = []
 
         # Inner lines
         for i in range(0, len(self.road)):
-            if (i == len(self.road) - 1):
+            if i == len(self.road) - 1:
                 self.lines.append(Line(self.road[i], self.road[0]))
             else:
                 self.lines.append(Line(self.road[i], self.road[i + 1]))
 
         # Outer points
-        outerPoints = []
+        outer_points = []
         for i in range(0, len(self.road)):
             pt1 = self.road[i]
-            if (i == len(self.road) - 1):
+            if i == len(self.road) - 1:
                 pt2 = self.road[0]
             else:
                 pt2 = self.road[i + 1]
@@ -37,30 +39,30 @@ class CarWindow(object):
             x = self.rw * math.sin(alpha)
             y = self.rw * math.cos(alpha)
 
-##            if (x_diff > 0):
-##                if (y_diff > 0):
-##                    outerPoints.append(Point(pt1.x - x, pt1.y - y))
-##                    outerPoints.append(Point(pt2.x - x, pt2.y - y))
-##                else:
-##                    outerPoints.append(Point(pt1.x - x, pt1.y + y))
-##                    outerPoints.append(Point(pt2.x - x, pt2.y + y))
-##            else:
-##                if (y_diff > 0):
-##                    outerPoints.append(Point(pt1.x + x, pt1.y - y))
-##                    outerPoints.append(Point(pt2.x + x, pt2.y - y))
-##                else:
-##                    outerPoints.append(Point(pt1.x + x, pt1.y + y))
-##                    outerPoints.append(Point(pt2.x + x, pt2.y + y))
+            ##            if (x_diff > 0):
+            ##                if (y_diff > 0):
+            ##                    outerPoints.append(Point(pt1.x - x, pt1.y - y))
+            ##                    outerPoints.append(Point(pt2.x - x, pt2.y - y))
+            ##                else:
+            ##                    outerPoints.append(Point(pt1.x - x, pt1.y + y))
+            ##                    outerPoints.append(Point(pt2.x - x, pt2.y + y))
+            ##            else:
+            ##                if (y_diff > 0):
+            ##                    outerPoints.append(Point(pt1.x + x, pt1.y - y))
+            ##                    outerPoints.append(Point(pt2.x + x, pt2.y - y))
+            ##                else:
+            ##                    outerPoints.append(Point(pt1.x + x, pt1.y + y))
+            ##                    outerPoints.append(Point(pt2.x + x, pt2.y + y))
 
-            outerPoints.append(Point(pt1.x - x, pt1.y - y))
-            outerPoints.append(Point(pt2.x - x, pt2.y - y))
+            outer_points.append(Point(pt1.x - x, pt1.y - y))
+            outer_points.append(Point(pt2.x - x, pt2.y - y))
 
         # Outer lines
-        for i in range(0, len(outerPoints)):
-            if (i == len(outerPoints) - 1):
-                self.lines.append(Line(outerPoints[i], outerPoints[0]))
+        for i in range(0, len(outer_points)):
+            if i == len(outer_points) - 1:
+                self.lines.append(Line(outer_points[i], outer_points[0]))
             else:
-                self.lines.append(Line(outerPoints[i], outerPoints[i + 1]))
+                self.lines.append(Line(outer_points[i], outer_points[i + 1]))
 
     def draw(self):
         for line in self.lines:
@@ -71,5 +73,5 @@ class CarWindow(object):
 carWindow = CarWindow()
 pts = [Point(100, 100), Point(300, 50), Point(500, 100), Point(400, 300),
        Point(150, 320)]
-carWindow.setRoad(pts)
+carWindow.set_road(pts)
 carWindow.draw()
