613c7cdf342764470dc05ce1b5dd9074fd6a3946
Tein van der Lugt <teinvdlugt.developer@gmail.com>
2015-12-09 15:54:21 +0100

    Update car.py

--- car.py
+++ car.py
@@ -2,9 +2,10 @@ class Car(object):
     def __init__(self, x, y, ann):
         self.x = x
         self.y = y
-        self.speed = 50  # px per second
+        self.speed = 1  # px per second
         self.direction = 0  # 0 = rechts, radialen
         self.ann = ann
+        self.collide_distance = -1
 
     def update_direction(self, sensor_data):
         self.direction += self.ann.propagate_forward(sensor_data)
@@ -12,3 +13,7 @@ class Car(object):
     def set_position(self, x, y):
         self.x = x
         self.y = y
+
+    def add_position(self, x, y):
+        self.x += x
+        self.y += y
