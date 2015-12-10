3c043405d57a2084098a2ec8a83608dea65445bb
Tein van der Lugt <teinvdlugt@gmail.com>
2015-12-10 21:00:20 +0100

    Create selector.py
    Dit is de evolueerder

--- car.py
+++ car.py
@@ -1,11 +1,24 @@
+from ANN import *
+
+
 class Car(object):
-    def __init__(self, x, y, ann):
+    def __init__(self, ann=None, parents=None, collide_distance=-1, direction=0, x=0, y=0):
         self.x = x
         self.y = y
         self.speed = 1  # px per second
-        self.direction = 0  # 0 = rechts, radialen
-        self.ann = ann
-        self.collide_distance = -1
+        self.direction = direction  # 0 = rechts, radialen
+        self.collide_distance = collide_distance
+
+        if ann is None:
+            if parents is None or len(parents) == 0:
+                self.ann = ANN()
+            else:
+                self.inherit_from(parents)
+        else:
+            self.ann = ann
+
+    def inherit_from(self, parents):
+        pass
 
     def update_direction(self, sensor_data):
         self.direction += self.ann.propagate_forward(sensor_data)
---
+++ selector.py
@@ -0,0 +1,46 @@
+# "Evolueerder"
+
+from car import *
+from road import *
+
+
+class Selector:
+    def __init__(self):
+        self.road = self.make_road()
+        self.cars = []
+        self.initial_generation()
+
+    def initial_generation(self):
+        # Add 50 random cars
+        self.cars = []
+        for i in range(50):
+            car = Car()
+            self.cars.append(car)
+
+    def create_next_generation(self):
+        # Select best performing cars
+        best_performing = []
+        for i in xrange(25):
+            best_car_index = 0
+            for c in range(len(self.cars)):
+                if self.cars[c].collide_distance > self.cars[best_car_index].collide_distance:
+                    best_car_index = c
+            best_performing.append(best_car_index)
+
+        # Create new generation with best_performing as parents
+        new_generation = []
+        for i in xrange(25):
+            new_generation.append(Car(best_performing))
+
+        # Add best_performing and new_generation to self.cars
+        self.cars = []
+        self.cars.append(car for car in best_performing)
+        self.cars.append(car for car in new_generation)
+
+    @staticmethod
+    def make_road():
+        new_road = Road()
+        points = [Point(100, 100), Point(300, 50), Point(500, 100), Point(501, 200), Point(450, 300),
+                  Point(300, 340), Point(150, 320), Point(50, 250)]
+        new_road.set_road(points)
+        return new_road
