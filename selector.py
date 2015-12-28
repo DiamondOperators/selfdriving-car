bc78ec6e56dcfd70a312202e636d52537d924eea
Tein van der Lugt <teinvdlugt@gmail.com>
2015-12-28 19:17:25 +0100

    Extract selector.population_size variable

--- selector.py
+++ selector.py
@@ -9,18 +9,19 @@ class Selector:
     def __init__(self):
         self.road = self.make_road()
         self.cars = []
+        self.population_size = 50  # Uit hoeveel auto's een populatie bestaat; moet een even getal zijn
 
     def initial_generation(self):
-        # Add 50 random cars
+        # Add {self.population_size} random cars
         self.cars = []
-        for i in xrange(0, 50):
+        for i in xrange(0, self.population_size):
             car = Car()
             self.cars.append(car)
 
     def create_next_generation(self):
         # Select best performing cars
         best_performing = []
-        for i in xrange(25):
+        for i in xrange(self.population_size / 2):
             best_car_index = 0
             for c in range(0, len(self.cars)):
                 if self.cars[c].collide_distance > self.cars[best_car_index].collide_distance:
@@ -29,7 +30,7 @@ class Selector:
 
         # Create new generation with best_performing as parents
         new_generation = []
-        for i in xrange(25):
+        for i in xrange(self.population_size / 2):
             new_generation.append(Car(best_performing))
 
         # Add best_performing and new_generation to self.cars
