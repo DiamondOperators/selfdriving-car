791b57113324f27d119c63c0f4f5b05584c9b7e4
Tein van der Lugt <teinvdlugt@gmail.com>
2016-01-20 15:39:05 +0100

    Create backpropagation things

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from ANN import *
 from selector import *
+from backpropagator import *
 
 ann = ANN()
 
 
 def func():
-    selector = Selector()
-    selector.initial_generation()
-    selector.test_generation()
-
-    while 1 + 1 == 2:
-        selector.road.reset_win()
-        selector.create_next_generation()
+    option = input("1) Neuroevolution\n2)Backpropagation\n")
+    if option == 1:
+        selector = Selector()
+        selector.initial_generation()
         selector.test_generation()
 
-    input("Press return to exit")
+        while 1 + 1 == 2:
+            selector.road.reset_win()
+            selector.create_next_generation()
+            selector.test_generation()
+    elif option == 2:
+        pass
+    else:
+        print "Please choose 1 or 2 or press Ctrl-C"
+        func()
 
 if __name__ == "__main__":
     func()
